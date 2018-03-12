8e49352938a8adcba1eeb2c5a1380da8bd182601
nadezhda.guschina <nadegda8808@gmail.com>
2018-03-12 22:10:46 +0300

    создан пакет для моей первой программы

--- sandbox/src/main/java/ru/stqa/nadya_firststeps/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/nadya_firststeps/sandbox/MyFirstProgram.java
@@ -1,4 +1,6 @@
-public class MyFirstProgram{
+package ru.stqa.nadya_firststeps.sandbox;
+
+public class MyFirstProgram {
 	
 	public static void main(String[] args) {
 	System.out.println("Hello,world");
